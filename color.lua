90190e291d5726b0e9d66512bd028167b9bc0dcb
Ted Dobyns <ted@tedajax.net>
2013-04-21 11:01:31 -0700

    made math.clamp local as not to confuse it with any global stuff later (potentially)

--- color.lua
+++ color.lua
@@ -1,6 +1,6 @@
 local assert = assert
 
-function math.clamp(val, lower, upper)
+local function math.clamp(val, lower, upper)
     if lower > upper then lower, upper = upper, lower end -- swap if boundaries supplied the wrong way
     return math.max(lower, math.min(upper, val))
 end
